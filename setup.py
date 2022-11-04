28a76128c0d68358e956f19f299297eb512ef713
TheRebellious <101460803+TheRebellious@users.noreply.github.com>
2022-11-04 15:09:58 +0100

    Library ready

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='HanPy',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A library with a bunch of handy functions',
+    author='TheRebellious, KingFingertips',
+    license='Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License',
+)
